165e51719a4dcf816dd6bd80b3da0c61b54bb7af
mpetermann <mpetermann@waukesha.k12.wi.us>
2022-10-10 11:58:44 -0500

    rename controller

--- src/GameController.java
+++ src/GameController.java
@@ -1,4 +1,4 @@
-public class Main {
+public class GameController {
     public static void main(String[] a) {
         GameSpinner spinner = new GameSpinner(4);
         System.out.println(spinner.spin());
