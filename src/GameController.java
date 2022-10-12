c005ac65928be498ce052f76af766c675509a683
mpetermann <mpetermann@waukesha.k12.wi.us>
2022-10-11 19:17:35 -0500

    output spin as prefix

--- src/GameController.java
+++ src/GameController.java
@@ -2,11 +2,11 @@ public class GameController {
     public static void main(String[] a) {
         GameSpinner spinner = new GameSpinner(4);
         System.out.println("Current Run: " + spinner.currentRun());
-        System.out.println(spinner.spin());
+        System.out.println("Spin: " + spinner.spin());
         System.out.println("Current Run: " + spinner.currentRun());
-        System.out.println(spinner.spin());
+        System.out.println("Spin: " + spinner.spin());
         System.out.println("Current Run: " + spinner.currentRun());
-        System.out.println(spinner.spin());
+        System.out.println("Spin: " + spinner.spin());
         System.out.println("Current Run: " + spinner.currentRun());
     }
 }
